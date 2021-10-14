793642980de302224cb7f7fcc25c3d6c0e75d819
Punk <s666punk@gmail.com>
2021-10-14 15:58:36 +0300

    add base backend structure

---
+++ backend/src/test/java/ru/netcracker/auckland/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netcracker.auckland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

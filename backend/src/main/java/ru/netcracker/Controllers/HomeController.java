c317bd34e4c4ac93146ee2aea46a19a4775d380d
GammelSmurf <vlad_korol69@mail.ru>
2021-10-14 17:29:26 +0300

    add frontend base

---
+++ backend/src/main/java/ru/netcracker/Controllers/HomeController.java
@@ -0,0 +1,18 @@
+package ru.netcracker.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+@RequiredArgsConstructor
+public class HomeController {
+    @GetMapping
+    public String home(){
+        return "Hello world!";
+    }
+}

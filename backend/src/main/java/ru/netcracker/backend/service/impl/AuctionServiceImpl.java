6aa064c9db431a8ab9c4d5ab85f51b276f6b8840
s666p <s666punk@gmail.com>
2021-10-19 01:23:35 +0300

    delete exceptions

--- backend/src/main/java/ru/netcracker/backend/service/impl/AuctionServiceImpl.java
+++ backend/src/main/java/ru/netcracker/backend/service/impl/AuctionServiceImpl.java
@@ -2,10 +2,10 @@ package ru.netcracker.backend.service.impl;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import ru.netcracker.backend.exception.ResourceNotFoundException;
 import ru.netcracker.backend.models.Auction;
 import ru.netcracker.backend.repository.AuctionRepo;
 import ru.netcracker.backend.service.AuctionService;
@@ -28,7 +28,7 @@ public class AuctionServiceImpl implements AuctionService {
     @Override
     public ResponseEntity<Auction> getAuction(long auctionId) {
         Auction auction = auctionRepo.findById(auctionId)
-                .orElseThrow(() -> new ResourceNotFoundException("Auction", "AuctionId", auctionId));
+                .orElseThrow(() -> new ResourceNotFoundException("Auction with id " + auctionId + " was not found."));
         return new ResponseEntity<>(auction, HttpStatus.FOUND);
     }
 }

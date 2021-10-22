987ef839040f3b10072c3e42e4d0407eb3bc3740
s666p <s666punk@gmail.com>
2021-10-22 20:57:07 +0300

    replace one-page web app config with redirect controller

---
+++ backend/src/main/java/ru/netcracker/backend/controller/RedirectController.java
@@ -0,0 +1,14 @@
+package ru.netcracker.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectController {
+
+    @GetMapping(value = {"/{regex:\\w+}", "/**/{regex:\\w+}"})
+    public String forward404() {
+        return "forward:/";
+    }
+
+}

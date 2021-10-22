9b0986399abe7709e2a44c14c3c054b6ea2abf71
s666p <s666punk@gmail.com>
2021-10-23 01:18:02 +0300

    fix 403 error in console

--- backend/src/main/java/ru/netcracker/backend/controller/RedirectController.java
+++ backend/src/main/java/ru/netcracker/backend/controller/RedirectController.java
@@ -1,8 +1,10 @@
 package ru.netcracker.backend.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class RedirectController implements ErrorController {
@@ -10,6 +12,7 @@ public class RedirectController implements ErrorController {
     private static final String PATH = "/error";
 
     @RequestMapping(value = PATH)
+    @ResponseStatus(HttpStatus.OK)
     public String error() {
         return "forward:/index.html";
     }

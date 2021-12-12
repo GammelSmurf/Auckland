63e37a4193ae82a0389aaf96277ffdebcb424e41
Alexandr <alex.pes4@vk.com>
2021-12-12 15:44:37 +0300

    add creating a category for admin

--- backend/src/main/java/ru/netcracker/backend/controller/AdminController.java
+++ backend/src/main/java/ru/netcracker/backend/controller/AdminController.java
@@ -9,12 +9,15 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.netcracker.backend.exception.user.UserExistsException;
 import ru.netcracker.backend.model.requests.CurrencyRequest;
+import ru.netcracker.backend.model.responses.CategoryResponse;
 import ru.netcracker.backend.model.responses.UserResponse;
+import ru.netcracker.backend.service.CategoryService;
 import ru.netcracker.backend.service.NotificationService;
 import ru.netcracker.backend.service.UserService;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @RestController
@@ -26,12 +29,14 @@ public class AdminController {
     private final ModelMapper modelMapper;
     private final UserService userService;
     private final NotificationService notificationService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public AdminController(ModelMapper modelMapper, UserService userService, NotificationService notificationService) {
+    public AdminController(ModelMapper modelMapper, UserService userService, NotificationService notificationService, CategoryService categoryService) {
         this.modelMapper = modelMapper;
         this.userService = userService;
         this.notificationService = notificationService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping
@@ -59,5 +64,12 @@ public class AdminController {
         UserResponse userResponse = userService.addCurrency(currencyRequest.getUsername(), currencyRequest.getCurrency());
         log.info("added {}$ to user {}", currencyRequest.getCurrency(), currencyRequest.getUsername());
         return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/category/add")
+    public ResponseEntity<?> addCategory(@NotBlank @Size(min = 3, max = 255) @RequestBody String categoryName) {
+        CategoryResponse categoryResponse=categoryService.addCategory(categoryName);
+        log.info("added category with name {}",categoryName);
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 }
--- backend/src/main/java/ru/netcracker/backend/service/CategoryService.java
+++ backend/src/main/java/ru/netcracker/backend/service/CategoryService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface CategoryService {
     List<CategoryResponse> getAllCategories();
+    CategoryResponse addCategory(String categoryName);
 }
